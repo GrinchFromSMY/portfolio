c6ae46491c7e6e0ddc1423f3ddddbf1f54f6b3c6
GrinchFromSMY <dota4case2@gmail.com>
2024-06-12 21:22:38 +0200

    New functions

--- run.py
+++ run.py
@@ -1,6 +1,16 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from flask_mail import Mail, Message
 
 app = Flask(__name__)
+
+app.config['MAIL_SERVER'] = 'smtp.gmail.com'
+app.config['MAIL_PORT'] = 465
+app.config['MAIL_USERNAME'] = 'exapmle@gamil.com'
+app.config['MAIL_PASSWORD'] = '123123'
+app.config['MAIL_USE_TLS'] = False
+app.config['MAIL_USE_SSL'] = True
+mail = Mail(app)
+
 
 @app.route('/')
 def home():
@@ -14,8 +24,13 @@ def about_me():
 def gallery():
     return render_template('gallery.html')
 
-@app.route('/contact')
+@app.route('/contact', methods=['GET', 'POST'])
 def contact():
+    if request.method == 'POST':
+        msg = Message("Hello, I'm from Portfolio!", sender='noreply@demo.com', recipients=['daniilmazurenko2204@gmail.com'])
+        msg.body = "Hey, how are you? Is everything okay?"
+        mail.send(msg)
+        return "Email sent!"
     return render_template('contact.html')
 
 @app.errorhandler(404)
